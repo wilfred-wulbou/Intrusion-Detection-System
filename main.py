4b7e68e6b602c62078f84fa787f4796d87fa2d7c
wilfred-wulbou <wwulbou@gmail.com>
2019-09-29 00:24:08 +1000

    Updated main.py and CSVFlowLoader class

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-from prepare_dumps import dump_pipeline
+import time
+import os
+from flow_loader.csv_flow_loader import CSVFlowLoader
+
+CSVFILEPATH = "CICFlowMeter-4.0\\bin\\data\\daily"
+csvfilename = "2019-09-29_Flow.csv"
+readBuffer=[] 
+
+def runIDS():
+    print("Starting IDS...")
+    
+    try:
+        csvloader = CSVFlowLoader(os.path.join(CSVFILEPATH, csvfilename))
+        while True:
+            for line in csvloader.tailFile():
+                print(line)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Exiting...")
+        csvloader.destroy()
+    
 
 if __name__ == "__main__":
-    dump_pipeline.dump()
+    runIDS()
